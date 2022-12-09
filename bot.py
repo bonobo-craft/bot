7d258aaeaa1e734d275c3d03bc2cdf01b8522a1d
Pure Luck <68428243+bonobo-craft@users.noreply.github.com>
2022-12-10 00:05:26 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ load_dotenv()
 stability_api = client.StabilityInference(
     key=os.environ['STABLE_DIFFUSION_TOKEN'],
     verbose=True,
-    engine="stable-diffusion-v1-5", # Set the engine to use for generation. 
+    engine="stable-diffusion-512-v2-0", # Set the engine to use for generation. 
     # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0 
     # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-inpainting-v1-0 stable-inpainting-512-v2-0
 )
